103cab3e511594ca463ec790720287888a555781
Henry Rufino <rufino.hlr@mail.com>
2025-06-17 21:07:11 -0300

    Correcao do Vercel

--- app/page.tsx
+++ app/page.tsx
@@ -101,7 +101,7 @@ export default function Home() {
               </p>
             </div>
             <div className={styles.aboutPhoto}>
-              <Image src="/cpu.png" alt="Henry Rufino" width={450} height={250} className={styles.imgAnimada} />
+              <Image src="/cpu2.png" alt="Henry Rufino" width={450} height={250} className={styles.imgAnimada} />
             </div>
           </div>
         </section>
